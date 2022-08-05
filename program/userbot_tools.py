06839b5ea155aa62e2c41f433694d810754d94a3
SiriamUsic <110149686+SiriamUsic@users.noreply.github.com>
2022-08-05 15:58:32 +0200

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -21,9 +21,9 @@ async def join_chat(c: Client, m: Message):
                 "https://t.me/+", "https://t.me/joinchat/"
             )
             await user.join_chat(invitelink)
-            return await user.send_message(chat_id, "انضممت هنا كما طلبت")
+            return await user.send_message(chat_id, "سامو عليكو اعملولي شاي كدا الاول 😂✈️")
     except UserAlreadyParticipant:
-        return await user.send_message(chat_id, "انا بالفعل موجود هنا 😐")
+        return await user.send_message(chat_id, "انا هنا اهو يا اعمى 😂✈️")
 
 
 @Client.on_message(
@@ -36,12 +36,12 @@ async def leave_chat(_, m: Message):
         await user.leave_chat(chat_id)
         return await _.send_message(
             chat_id,
-            "✅ غادر الحساب المساعد المجموعه بنجاح",
+            "المساعد طفش منكم لله 😂✈️",
         )
     except UserNotParticipant:
         return await _.send_message(
             chat_id,
-            "❌ غادر الحساب المساعد المجموعه بالفعل",
+            "دا ماشي من بدري انت لسه فاكر 🙃",
         )
 
 
